968088be7fb91ef89cce13bc05290e815e71a4b9
Mafalda Nunes <gogreen.m@hotmail.com>
2023-09-01 18:19:06 +0200

    ItemRepository

---
+++ src/Catalog.Service/Entities/Item.cs
@@ -0,0 +1,13 @@
+namespace Entities
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
